61c618186389fdf69d5fd4a6c5c4b9c669cbb02e
Scott Larsen <sslarsen411@gmail.com>
2022-01-10 08:54:01 -0800

    Add about text, cleanup js code

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,15 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const btnSubmit      = document.querySelector('button[type="submit"]')
-//const para = document.querySelectorAll('p')
 const messageOne = document.querySelector('#fb-1')
 const messageTwo = document.querySelector('#fb-2')
 
-//console.log(para)
-//let x = weatherForm
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    //console.log(search.value)
+    
     const addr = encodeURIComponent(search.value)
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-
-    //console.log(addr)
      
     fetch('/weather?address=' + addr).then((response) => {
         response.json().then((data) => {
@@ -28,23 +23,3 @@ weatherForm.addEventListener('submit', (e) => {
     })
 
 })
-
-
-
-// fetch('https://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-
-// })
-
-// fetch('http://localhost:3000/weather?address=boston').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data.title)
-//             console.log(data.forecast)
-//         }
-//     })
-// })
